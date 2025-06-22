b0d0f6e2c984dc2266c3a95da448c8586d389f2c
ocean-nest <siyaramlakshman24@gmail.com>
2025-06-22 22:24:31 +0530

    programs

---
+++ javaPrograms/leetcode/array/ContiguousArray.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContiguousArray {
+
+    public static void main(String args[]) {
+
+        int[] nums = {1,1,0,0,1,1,1,1,0,1,1,0,0,0,1};
+        System.out.println(new Sloution().findMaxLength(nums));
+    }
+
+}
+
+ class Sloution {
+    public int findMaxLength(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int maxLength = 0;
+        int counter = 0;
+
+
+        for (int i=0; i< nums.length; i++ ) {
+
+            if(nums[i] == 1) {
+                counter++;
+            } else {
+                counter--;
+            }
+
+            if(map.containsKey(counter)) {
+                maxLength = Math.max(i - map.get(counter), maxLength);
+            } else {
+                map.put(counter, i);
+            }
+        }
+
+        return maxLength;
+
+
+    }
+}
---
+++ javaPrograms/leetcode/array/SubarraySumEqualsK.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+
+        //int[] nums = {1,1,0,0,1,1,1,1,0,1,1,0,0,0,1};
+        int[] nums = {1,1,1,1,1,1,1};
+        System.out.println(new Solution().subarraySum(nums, 3));
+    }
+}
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int kcount = 0;
+        int counter = 0;
+
+
+        for (int i: nums ) {
+            counter += i;
+
+            if(map.containsKey(counter-k)) {
+                kcount += map.get(counter-k);
+            }
+
+            map.put(counter, map.getOrDefault(counter, 0) + 1);
+        }
+
+        System.out.println(map);
+
+        return kcount;
+    }
+}
+
